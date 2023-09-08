9c8fff3b088cb455e8de162e2f1e14b253a200e9
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-09-08 12:04:10 -0700

    Create Maximum Product Subarray.py

---
+++ 1-D DP/Maximum Product Subarray.py
@@ -0,0 +1,15 @@
+### this doesnt fill like dp
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+
+        A = nums
+        B = A[::-1]
+        for i in range(1,len(nums)):
+            A[i]*= A[i-1] or 1
+            B[i]*= B[i-1] or 1
+
+        return max(A+B)    
+
+
+
+        
