d1b2a4e298c50c6650393ee8c57c263b72a9dde6
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-09-08 22:43:32 -0700

    Create work break.py

---
+++ 1-D DP/work break.py
@@ -0,0 +1,54 @@
+#### dfs + memo
+### This solution is reserved of how we implement dp is from the dict to s rather than s to wordict
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        
+
+        invaild = set()
+
+        def dfs(s):
+
+            nonlocal invaild
+
+            if not s:
+                return True
+            if s in invaild:
+                return False
+
+            for w in wordDict:
+                if (s.startswith(w) and dfs(s[len(w):])):
+                    return True
+            invaild.add(s)
+            return False
+
+        return dfs(s)  
+
+
+### 1 DP
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        dp = [False]*(len(s)+1)
+        dp[len(s)] = True
+
+        for i in range(len(s)-1,-1,-1):
+            for w in wordDict:
+                if (i+len(w)<=len(s)) and s[i:i+len(w)]==w:
+                    dp[i]=dp[i+len(w)]
+                if dp[i]:
+                    break
+        return dp[0]                
+        
+               
+
+
+
+                
+
+                
+
+
+        
