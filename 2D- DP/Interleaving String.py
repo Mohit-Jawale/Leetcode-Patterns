fd9955831cf89fc8048db9e8907ec1b7b4ec7d76
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-09-13 16:09:23 -0700

    Update Interleaving String.py

--- 2D- DP/Interleaving String.py
+++ 2D- DP/Interleaving String.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         
@@ -7,13 +8,16 @@ class Solution:
         if len(s1)+len(s2)!=len(s3):
             return False
 
-        dp[len(s1)][len(s2)]=1    
+        dp[0][0]=True
 
-        for i in range(len(s1),-1,-1):
-            for j in range(len(s2),-1,-1):
-                if i<len(s1) and s3[i+j]==s1[i] and dp[i+1][j]==True:
-                    dp[i][j] = True
-                if j<len(s2) and s3[i+j]==s2[j] and dp[i][j+1]==True:
-                    dp[i][j] = True
+        for i in range(1,len(s1)+1):
+            dp[i][0] = dp[i-1][0] and s1[i-1]==s3[i-1]
 
-        return dp[0][0]                
+        for j in range(1,len(s2)+1):
+            dp[0][j] = dp[0][j-1] and s2[j-1]==s3[j-1]   
+
+        for i in range(1, len(s1)+1):
+            for j in range(1,len(s2)+1):
+                dp[i][j]= (dp[i-1][j] and s3[i+j-1]==s1[i-1])or(dp[i][j-1] and s3[i+j-1]==s2[j-1])     
+        return dp[-1][-1]
+
