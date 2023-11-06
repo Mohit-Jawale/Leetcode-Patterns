7cdc8e241505731e5e3c5ea52faf581329086bad
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-11-06 08:02:42 -0800

    Update Coin Change II.py

--- 2D- DP/Coin Change II.py
+++ 2D- DP/Coin Change II.py
@@ -1,4 +1,25 @@
 class Solution:
+    ### This problem talks about unique combinations like 1,1,2!= 2,1,1 thats why we have to use i
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        memo = {}
+        def dfs(k,amount):
+            if amount == 0:
+                return 1
+            if (k,amount) in memo:
+                return memo[(k,amount)]
+            count = 0
+            for i in range(k,len(coins)):
+                if amount>=coins[i]:
+                    count += dfs(i,amount-coins[i])
+            memo[(k,amount)]=count
+            return count  
+        return dfs(0,amount)
+                
+
+
+
+class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
 
         dp = [[0 for j in range(amount+1)] for _ in range(len(coins))]
