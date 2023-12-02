812f3b25f90207713e5eae3e884d9a6c43b6dd20
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-01 17:22:16 -0800

    Update Minimum Path Sum.py

--- Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Minimum Path Sum.py
+++ Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Minimum Path Sum.py
@@ -24,4 +24,26 @@ class Solution:
             return minSum
 
         return dfs(m,n)    
+
+## This Bottom-down
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+
+        m = len(grid)
+        n = len(grid[0])
+        
+
+        for i in range(0,m):
+            for j in range(0,n):
+                if i==0 and j==0:
+                    continue
+                if i==0:
+                    grid[i][j]+=grid[i][j-1]
+                elif j==0:
+                    grid[i][j]+=grid[i-1][j]
+                else:            
+                    grid[i][j] = min(grid[i-1][j],grid[i][j-1]) + grid[i][j] 
+
+        return grid[m-1][n-1]        
         
