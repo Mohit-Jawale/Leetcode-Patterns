73f145f633ae1515e595db9c575c24b27dbe9a91
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-01 18:57:23 -0800

    Update Coin Change.py

--- Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Coin Change.py
+++ Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Coin Change.py
@@ -23,3 +23,20 @@ class Solution:
 
         ans = dfs(amount)   
         return ans if ans != float('inf') else -1
+
+## 1D- DPb bottom up
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        dp = [float('inf')]*(amount+1)
+
+        dp[0]=0
+        
+        for amount in range(1,amount+1):
+            for coin in coins:
+                if coin<=amount:
+                    dp[amount]=min(dp[amount],dp[amount-coin]+1)
+
+        return dp[-1] if dp[-1]!=float('inf') else -1          
+
