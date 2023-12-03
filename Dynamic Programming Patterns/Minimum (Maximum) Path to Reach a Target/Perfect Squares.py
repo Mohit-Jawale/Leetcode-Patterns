b836cf0fe824b4fcd133d8d8e46f1743ef0d1c7f
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-03 13:16:33 -0800

    Update Perfect Squares.py

--- Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Perfect Squares.py
+++ Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Perfect Squares.py
@@ -22,4 +22,26 @@ class Solution:
             return minSQ  
 
         return dfs(n)            
-        
+
+### 2 dp
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+
+        perfectSquares = [i for i in range(1,n+1) if int(sqrt(i))*int(sqrt(i))==i]
+
+        dp = [[0 for col in range(n+1)] for row in range(len(perfectSquares))]
+
+        for i in range(len(dp)):
+            for j in range(len(dp[0])):
+
+                if i==0 or j==0:
+                    dp[i][j]=j
+                elif j==0:
+                    continue
+                elif j>=perfectSquares[i]:
+                     dp[i][j]= min(dp[i-1][j],dp[i][j-perfectSquares[i]]+1)
+                else:
+                    dp[i][j]= dp[i-1][j]    
+
+        return dp[-1][-1]
