77daff66a95afaa41486b78fc58a2eaba277600b
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-05 11:34:25 -0800

    Update Minimum Cost For Tickets.py

--- Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Minimum Cost For Tickets.py
+++ Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Minimum Cost For Tickets.py
@@ -1,23 +1,45 @@
+### here bisect is used because while creating decision tree I released I need next days in the days for which the array needs to travel
 class Solution:
-    def mincostTickets(self, days, costs):
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        
+
+        memo = {}
         def dfs(i):
-            if i == len(days):
+            
+            if i>=len(days):
                 return 0
-            if memo[i] != float('inf'):
+
+            if i in memo:
                 return memo[i]
+            minCost = min(dfs(i+1)+costs[0],dfs(bisect_left(days,days[i]+7))+costs[1],dfs(bisect_left(days,days[i]+30))+costs[2])
+            memo[i] = minCost
+            return minCost
 
-            ans = dfs(i + 1) + costs[0]
-            j = i
-            while j < len(days) and days[j] < days[i] + 7:
-                j += 1
-            ans = min(ans, dfs(j) + costs[1]
+        return dfs(0)     
 
-            while j < len(days) and days[j] < days[i] + 30:
-                j += 1
-            ans = min(ans, dp(j) + costs[2])
+## 1DP solution
+## the max trick is worth noting
 
-            memo[i] = ans
-            return ans
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
 
-        memo = [float('inf')] * len(days)
-        return dp(0)
+        dp = [0]*(days[-1]+1)
+        days = set(days)
+        for day in range(1,len(dp)):
+            
+            if day not in days:
+                dp[day]=dp[day-1]
+            else:
+                cost1 = dp[max(day-1,0)]+costs[0]
+                cost7 = dp[max(day-7,0)]+costs[1]
+                cost30 = dp[max(day-30,0)]+costs[2]
+
+                dp[day]=min(cost1,cost7,cost30)
+
+        return dp[-1]        
+
+
+
+
+
+        
