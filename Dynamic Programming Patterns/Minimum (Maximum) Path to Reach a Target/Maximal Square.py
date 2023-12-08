d6a8010e9ad072250ab0f32e94f2f66f2ebe597f
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-08 12:28:16 -0800

    Update Maximal Square.py

--- Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Maximal Square.py
+++ Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Maximal Square.py
@@ -31,6 +31,22 @@ class Solution:
 
 
         return maxSq * maxSq         
+## 2 DP solution
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+
+        n,m = len(matrix[0]),len(matrix)
+        dp = [[0 for j in range(n+1)] for i in range(m+1)]
+
+        max_area = 0
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if matrix[i-1][j-1]=="1":
+                    dp[i][j]=min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+                    max_area = max(max_area,dp[i][j])
+        
+        return max_area*max_area
 
 
             
