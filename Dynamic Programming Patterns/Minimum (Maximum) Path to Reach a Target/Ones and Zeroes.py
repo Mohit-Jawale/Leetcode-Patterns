0dec016e103575d0745967fff1865675745d9a9e
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-09 09:16:57 -0800

    Update Ones and Zeroes.py

--- Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Ones and Zeroes.py
+++ Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Ones and Zeroes.py
@@ -24,5 +24,32 @@ class Solution:
             memo[(i,m,n)] = max(takenStr,dropStr)
             return max(takenStr,dropStr)
 
-        return dfs(0,m,n)    
+        return dfs(0,m,n)
+## 3-DP solution
+
+class Solution:
+    def findMaxForm(self, strs, m, n):
+        # Function to count the number of 1s and 0s in a binary string
+        def binary_count(binaryString):
+            count_1 = binaryString.count('1')
+            count_0 = binaryString.count('0')
+            return count_0, count_1
+
+
+        dp = [[[0 for _ in range(m+1)]for _ in range(n+1)]for _ in range(len(strs)+1)]
+
+        for i in range(1,len(strs)+1):
+            zeros,ones = binary_count(strs[i-1])
+            for j in range(n+1):
+                for k in range(m+1):
+                    if zeros<=k and ones<=j:
+                        dp[i][j][k]= max(dp[i-1][j-ones][k-zeros]+1,dp[i-1][j][k])
+                    else:
+                        dp[i][j][k]=dp[i-1][j][k]    
+ 
+        return dp[-1][-1][-1]                
+
+
+        
+
         
