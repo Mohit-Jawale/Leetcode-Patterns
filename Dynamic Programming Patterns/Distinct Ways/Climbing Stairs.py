cc615b92126638af1fb68d532f6661de302ffb98
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-09 15:56:23 -0800

    Update Climbing Stairs.py

--- Dynamic Programming Patterns/Distinct Ways/Climbing Stairs.py
+++ Dynamic Programming Patterns/Distinct Ways/Climbing Stairs.py
@@ -27,12 +27,12 @@ class Solution:
 
         if n==1:
             return 1
-
         dp = [0]*(n+1)
-        dp[1]=1
-        dp[2]=2
 
-        for i in range(3,len(dp)):
+        dp[0]=1
+        dp[1]=1
+
+        for i in range(2,len(dp)):
             dp[i]= dp[i-1]+dp[i-2]
 
         return dp[-1]
