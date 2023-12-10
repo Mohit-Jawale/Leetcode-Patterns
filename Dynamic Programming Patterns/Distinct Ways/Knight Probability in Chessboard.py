3c1bbe1d9b05f9b9cd633470d5d5cd0c9e7d3fc3
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-10 10:33:40 -0800

    Update Knight Probability in Chessboard.py

--- Dynamic Programming Patterns/Distinct Ways/Knight Probability in Chessboard.py
+++ Dynamic Programming Patterns/Distinct Ways/Knight Probability in Chessboard.py
@@ -31,5 +31,23 @@ class Solution:
         return dfs(row,column,k)    
 
 
+##  3d - dp 
+        
+        dp = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(k+1)]
+
+        moves = [(2, 1), (1, 2), (-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, -2), (2, -1)]
+
+        dp[0][row][column] = 1
+
+        for move in range(1,k+1):
+            for i in range(n):
+                for j in range(n):
+                    for dr,dc in moves:
+                        if 0 <= i + dr < n and 0 <= j + dc < n:
+                            dp[move][i][j] += dp[move - 1][i + dr][j + dc] / 8
+
+        return sum(dp[k][r][c] for r in range(n) for c in range(n)) 
+
+
 
 
