aa58d3f4b0bee0c48994a9a033e5b4786a810fe2
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-12 19:16:24 -0800

    Update Knight Dialer.py

--- Dynamic Programming Patterns/Distinct Ways/Knight Dialer.py
+++ Dynamic Programming Patterns/Distinct Ways/Knight Dialer.py
@@ -39,41 +39,44 @@ class Solution:
                     ans+=dfs(i,j,n) % MOD
 
         return ans % MOD
-### 3d- dp solution
+
+### 2d-dp solution here observer the loops are reversed
 
 class Solution:
     def knightDialer(self, n: int) -> int:
-        nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']]
-        moves = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
-        ROW, COL = len(nums), len(nums[0])
-        MOD = 10**9 + 7
 
-        # Initialize DP table
-        dp = [[[0 for _ in range(n + 1)] for _ in range(COL)] for _ in range(ROW)]
+        nums = [[1,2,3],[4,5,6],[7,8,9],['*',0,'#']]
+        moves = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[-1,2],[1,-2],[-1,-2]]
 
-        # Base cases
-        for i in range(ROW):
-            for j in range(COL):
-                if nums[i][j] not in ['*', '#']:
-                    dp[i][j][1] = 1
+        ROW,COL = len(nums),len(nums[0])
 
-        # Fill the DP table
-        for step in range(2, n + 1):
-            for i in range(ROW):
-                for j in range(COL):
-                    if nums[i][j] not in ['*', '#']:
-                        for r, c in moves:
-                            ni, nj = i + r, j + c
-                            if 0 <= ni < ROW and 0 <= nj < COL:
-                                dp[i][j][step] = (dp[i][j][step] + dp[ni][nj][step - 1]) % MOD
+        MOD = pow(10,9)+7
 
-        # Sum up the results
-        ans = 0
-        for i in range(ROW):
-            for j in range(COL):
-                ans = (ans + dp[i][j][n]) % MOD
+                    
+        dp = [[0 for _ in range(n+1)] for _ in range(10)]
+        
+        dp_to_num_mapper = {}
+        for r in range(ROW):
+            for c in range(COL):
+                if nums[r][c] not in ['*','#']:
+                    dp_to_num_mapper[nums[r][c]]=(r,c)
 
-        return ans
+        for i in range(10):
+            dp[i][1]=1
 
+        ### here when we draw the table we realised that we need prev col first to fill the n
+        ### thats y we swap the loop... the iteration in vertical rather than horizontal.
+        for j in range(2,n+1):
+            for i in range(10):
+                ar,ac = dp_to_num_mapper[i]
+                for r,c in moves:
+                    if 0<= ar+r < ROW and 0<= ac+c <COL and nums[ar+r][ac+c] not in ['*','#']:
+                        dp[i][j]+=dp[nums[ar+r][ac+c]][j-1] % MOD
+
+        totalNo = 0 
+        for i in range(len(dp)):
+            totalNo+=dp[i][-1] % MOD
+
+        return  totalNo % MOD               
 
         
