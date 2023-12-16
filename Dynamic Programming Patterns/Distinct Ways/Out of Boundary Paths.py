b591583064d2531375ba4e76998023e3e748f880
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-15 17:27:09 -0800

    Update Out of Boundary Paths.py

--- Dynamic Programming Patterns/Distinct Ways/Out of Boundary Paths.py
+++ Dynamic Programming Patterns/Distinct Ways/Out of Boundary Paths.py
@@ -25,4 +25,26 @@ class Solution:
 
             return totalMoves
 
-        return dfs(startRow,startColumn,maxMove) % MOD   
+        return dfs(startRow,startColumn,maxMove) % MOD
+
+
+
+## 3d dp 
+### draw the state and table to understand the recurrance relation
+        dp = [[[0 for _ in range(maxMove+1)]for _ in range(n)] for _ in range(m)]
+
+        dp[startRow][startColumn][0] = 1
+        count = 0
+
+        for k in range(maxMove):
+            for i in range(m):
+                for j in range(n):
+                    if dp[i][j][k]>0:
+                        for r,c in moves:
+                            ai,aj = i+r,j+c
+                            if 0<=ai<m and 0<=aj<n:
+                                dp[ai][aj][k+1]+=dp[i][j][k] % MOD
+                            else:
+                                count+=dp[i][j][k] % MOD
+
+        return count % MOD
