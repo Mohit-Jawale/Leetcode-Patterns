f255db280b3ac24cbf3b92ba5687e11d1b580998
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-17 09:46:02 -0800

    Update Unique Binary Search Trees.py

--- Dynamic Programming Patterns/Merging Intervals/Unique Binary Search Trees.py
+++ Dynamic Programming Patterns/Merging Intervals/Unique Binary Search Trees.py
@@ -22,6 +22,21 @@ class Solution:
 
             return totalBST
 
-        return dfs(n)        
+        return dfs(n)
+
+### 1d -dp
+        dp = [0] * (n+1)
+
+        dp[0]=dp[1]=1
+
+        for i in range(2,n+1):
+            total = 0
+            for j in range(1,i+1):
+                left = j-1
+                right = i-j
+                total+= dp[left]*dp[right]
+            dp[i] = total    
+
+        return dp[n]   
 
         
