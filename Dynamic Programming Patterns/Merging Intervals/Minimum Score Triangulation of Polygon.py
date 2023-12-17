4ba741749da7565222213439aaeb66fe84af7338
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-17 13:51:15 -0800

    Update Minimum Score Triangulation of Polygon.py

--- Dynamic Programming Patterns/Merging Intervals/Minimum Score Triangulation of Polygon.py
+++ Dynamic Programming Patterns/Merging Intervals/Minimum Score Triangulation of Polygon.py
@@ -20,4 +20,19 @@ class Solution:
 
         return dfs(0,len(values)-1)       
 
+### 2d-dp
+        
+        n = len(values)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+
+        for i in reversed(range(n)):
+            for j in range(i+2,n):
+
+                totalMIN = float('inf')
+                for k in range(i+1,j):
+                    totalMIN = min((values[i]*values[j]*values[k])+dp[i][k]+dp[k][j],totalMIN)
+
+                dp[i][j] = totalMIN
+        
+        return dp[0][n-1]    
         
