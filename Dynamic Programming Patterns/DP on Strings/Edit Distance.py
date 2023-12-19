3ccc13df65daf078b0edd35ff0c1922560193bbf
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-19 12:41:15 -0800

    Update Edit Distance.py

--- Dynamic Programming Patterns/DP on Strings/Edit Distance.py
+++ Dynamic Programming Patterns/DP on Strings/Edit Distance.py
@@ -32,6 +32,28 @@ class Solution:
             return minOp
 
         return dfs(0,0)  
+
+
+### 2d-dp 
+        dp = [[float('inf') for _ in range(n+1)] for _ in range(m+1)]
+
+        for j in range(n+1):
+            dp[m][j] = n-j
+
+        for i in range(m+1):
+            dp[i][n]=m-i    
+
+        
+        for i in reversed(range(m)):
+            for j in reversed(range(n)):
+                if word1[i]==word2[j]:
+                    dp[i][j]= dp[i+1][j+1]
+                else:
+                    dp[i][j]=min(dp[i+1][j]+1,dp[i][j+1]+1,dp[i+1][j+1]+1,dp[i][j]) 
+
+
+        return dp[0][0]             
+
                        
 
         
