a67aab1b80bb40aaadc8c4f9c834cc0d113ad000
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-19 10:26:28 -0800

    Create Shortest Common Supersequence.py

---
+++ Dynamic Programming Patterns/DP on Strings/Shortest Common Supersequence.py
@@ -0,0 +1,38 @@
+### There are multiple concepts involved like LCS,printing LCS, and modified LCS
+### This is good problem to understand lots of concpets related to LCS
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+
+        m,n = len(str1),len(str2)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+
+        for i in reversed(range(m)):
+            for j in reversed(range(n)):
+
+                if str1[i]==str2[j]:
+                    dp[i][j]= dp[i+1][j+1]+1
+                else:
+                    dp[i][j]=max(dp[i+1][j],dp[i][j+1])
+
+        i,j=0,0
+        lcs = ""
+        while i<m and j<n:
+                if str1[i]==str2[j]:
+                    lcs+=str1[i]
+                    i,j = i+1,j+1
+                else:
+                    if dp[i+1][j]>=dp[i][j+1]:
+                        lcs+=str1[i]
+                        i,j= i+1,j
+                    else:
+                        lcs+=str2[j]
+                        i,j = i,j+1
+
+        while i<m:
+            lcs+=str1[i]
+            i+=1
+        while j<n:
+            lcs+=str2[j]
+            j+=1
+   
+        return(lcs)                    
