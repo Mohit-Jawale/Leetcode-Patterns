2c1cb9d67ebc452443081eef1d2fb6402b7e960a
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-22 10:30:42 -0800

    Create Best Time to Buy and Sell Stock II.py

---
+++ Dynamic Programming Patterns/Decision Making/Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,33 @@
+### dfs + memo
+### this is good starting probleming for decision making and state analysis
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        memo = {}
+
+        def dfs(i,buy):
+
+            if (i,buy) in memo:
+                return memo[(i,buy)]
+
+            if i>=len(prices):
+                return 0
+            
+            if buy:
+                ### decide to buy
+                profit = -prices[i]+dfs(i+1,False)
+                skipBuy = dfs(i+1,True)
+                profit = max(profit,skipBuy)
+            else:
+                ### decide to sell
+                profit = prices[i]+dfs(i+1,True)
+                skipSell = dfs(i+1,False)
+                profit = max(profit,skipSell)
+            
+            memo[(i,buy)] = profit
+            return profit
+        
+        return dfs(0,True)
+
+
+        
