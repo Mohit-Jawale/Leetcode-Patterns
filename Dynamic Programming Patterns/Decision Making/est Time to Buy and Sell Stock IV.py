390c642c2767e8601447dfcbd3b94a1bdbdac6e9
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-22 13:22:20 -0800

    Create est Time to Buy and Sell Stock IV.py

---
+++ Dynamic Programming Patterns/Decision Making/est Time to Buy and Sell Stock IV.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+
+        n = len(prices)
+        memo = {}
+        def dfs(i,buy,transactions):
+
+            if (i,buy,transactions) in memo:
+                return memo[(i,buy,transactions)]
+
+            if i>=n or transactions>=k:
+                return 0
+
+            if buy:
+                profit = -prices[i] + dfs(i+1,False,transactions)
+                skipBuy = dfs(i+1,True,transactions)
+                profit = max(profit,skipBuy)
+            else:
+                profit = prices[i] + dfs(i+1,True,transactions+1)
+                skipSell = dfs(i+1,False,transactions)
+                profit = max(profit,skipSell)
+
+            memo[(i,buy,transactions)] = profit
+            return profit
+
+        return dfs(0,True,0)  
