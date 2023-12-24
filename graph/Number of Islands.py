a44d1dba61b40c47f30fd3d5725d0694b7e64ecc
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-12-24 13:27:48 -0800

    Update Number of Islands.py

--- graph/Number of Islands.py
+++ graph/Number of Islands.py
@@ -1,23 +1,56 @@
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        
 
-        R,C = len(grid), len(grid[0])
+        m,n = len(grid),len(grid[0])
 
-        number_of_island = 0
+        def dfs(i,j):
 
-        def dfs(r,c):
-            if grid[r][c]=='1': 
-                grid[r][c]='2'   
-                if r>=1:dfs(r-1,c)
-                if r+1<R:dfs(r+1,c)
-                if c>=1:dfs(r,c-1)
-                if c+1<C:dfs(r,c+1)
+            if i<0 or i>=m or j<0 or j>=n:
+                return
+
+            if grid[i][j]=="0" or grid[i][j]==2:
+                return 
+            
+            grid[i][j]=2
+
+            dfs(i+1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+            dfs(i-1,j)
+        
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=="1" and grid[i][j]!=2:
+                    dfs(i,j)
+                    count+=1
+        
+        return count
+
+
+
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+
+#         R,C = len(grid), len(grid[0])
+
+#         number_of_island = 0
+
+#         def dfs(r,c):
+#             if grid[r][c]=='1': 
+#                 grid[r][c]='2'   
+#                 if r>=1:dfs(r-1,c)
+#                 if r+1<R:dfs(r+1,c)
+#                 if c>=1:dfs(r,c-1)
+#                 if c+1<C:dfs(r,c+1)
 
         
-        for i in range(R):
-            for j in range(C):
-                if grid[i][j]!='2' and grid[i][j]=='1':
-                    dfs(i,j)
-                    number_of_island+=1
+#         for i in range(R):
+#             for j in range(C):
+#                 if grid[i][j]!='2' and grid[i][j]=='1':
+#                     dfs(i,j)
+#                     number_of_island+=1
 
-        return(number_of_island)
+#         return(number_of_island)
