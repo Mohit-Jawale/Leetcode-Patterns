d45416d320e9eda6feb61434670569ca150b6531
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-02-19 15:45:30 -0800

    Update Binary Search.py

--- binary search/Binary Search.py
+++ binary search/Binary Search.py
@@ -1,6 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+
+        ### this is normal bs
         left = 0
         right = len(nums)-1
         
@@ -17,4 +18,27 @@ class Solution:
                     left = middle+1
          
 
-        return -1                    
+        return -1  
+        
+
+#### This is upper bound variation
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left,right = 0,len(nums)
+
+        while left<right:
+            mid = (left+right)//2
+
+            if nums[mid]<=target:
+                left = mid+1
+            else:
+                right = mid
+        
+        if left>0 and nums[left-1]==target:
+            return left-1
+        else:
+            return -1
+
+        
+
