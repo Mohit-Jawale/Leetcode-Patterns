cef05d0fd58334812a2f24f5ba1e5ce5d9a132a0
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-02-22 12:16:34 -0800

    Update Coin Change.py

--- Dynamic Programming Patterns/revision/Coin Change.py
+++ Dynamic Programming Patterns/revision/Coin Change.py
@@ -22,12 +22,16 @@ class Solution:
         ans = dfs(0)
         return ans if ans != float('inf') else -1
 
+    #### how to convert
+        ### Step 1: make dp by analyising amount as we can see its 1-DP problem
+        ### Step 2: look for base case in your dfs set that in the above dp
+        ### Step 3: convert the dfs call or reccurance relation into dp and see how the flow is in top-down it will be opposite
 
 
       
-        dp = [float('inf')]*(amount+1)
-        dp[amount] = 0
-        for i in reversed(range(len(dp))):
+        dp = [float('inf')]*(amount+1) ### Step 1
+        dp[amount] = 0 ### Step 2
+        for i in reversed(range(len(dp))): ### step 3
             for j in range(len(coins)):
                 if i+coins[j]<=amount:
                     dp[i]=min(dp[i+coins[j]]+1,dp[i])
