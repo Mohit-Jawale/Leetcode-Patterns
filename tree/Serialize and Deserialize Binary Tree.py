ce287d0552df370913f5b8ec4421b259f2bb7652
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-03-01 08:37:09 -0800

    Update Serialize and Deserialize Binary Tree.py

--- tree/Serialize and Deserialize Binary Tree.py
+++ tree/Serialize and Deserialize Binary Tree.py
@@ -13,6 +13,73 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        serializedStr = []
+        queue = collections.deque([root])
+
+        while queue:
+            node = queue.popleft()
+            if node:
+                serializedStr.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                serializedStr.append('')
+        
+        return ",".join(serializedStr)
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return []
+        deserializeStr = data.split(',')
+        root = TreeNode(deserializeStr[0])
+        queue = collections.deque([root])
+        i=1
+        while queue:
+            node = queue.popleft()
+            if i<len(deserializeStr) and deserializeStr[i]:
+                node.left = TreeNode(int(deserializeStr[i]))
+                queue.append(node.left)
+            if i+1<len(deserializeStr) and deserializeStr[i+1]:
+                node.right = TreeNode(int(deserializeStr[i+1]))
+                queue.append(node.right)
+            i+=2
+        return root
+            
+            
+
+        
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
         ans = []
         def dfs(node):
             nonlocal ans
