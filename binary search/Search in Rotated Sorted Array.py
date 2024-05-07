4fefd533b05b36b6085c2a2add0b2d6857a9bba3
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-05-06 17:15:13 -0700

    Update Search in Rotated Sorted Array.py

--- binary search/Search in Rotated Sorted Array.py
+++ binary search/Search in Rotated Sorted Array.py
@@ -1,57 +1,103 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        def detect_rotation(left,right):
 
-            if nums[left]<=nums[right]:
-                return left
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
 
-            middle = 0
-            while left<=right:
 
-                middle = (left+right)//2
+        def find_pivot():
+            left,right = 0,len(nums)-1
 
-                if nums[middle]>nums[middle+1]:
-                    break
+            while left<right:
+                mid = left+(right-left)//2
+
+                if nums[mid]>nums[right]:
+                    left=mid+1
                 else:
-                    if nums[left] < nums[middle]:
-                        left = middle+1
-                    else:
-                        right = middle-1
+                    right = mid ### for if they are equal or its on the left side
+            
 
-            return middle+1
-
-        def binary_search(left,right,target):
-
+            return left
+        
+        def binary_search(left,right):
+            
             while left<=right:
-
-                middle = (left+right)//2
-
-                if  nums[middle]==target:
-                    return middle
+                mid = left+(right-left)//2
+                if nums[mid]==target:
+                    return mid
                 else:
-                    if nums[middle] >= target:
-                        right = middle-1
+                    if nums[mid]<target:
+                        left = mid+1
                     else:
-                        left = middle+1
+                        right = mid-1
+            
+            return -1
+        
+        pivot = find_pivot()
+        left,right= 0,len(nums)-1
 
-            return -1           
-
-        left, right = 0, len(nums)-1
-        pivot = detect_rotation(left,right)
-        print(pivot)
-
-        if nums[left]<=nums[right]:
-            return binary_search(left,right,target)
-        #### compare with target why? bcz if target is grter than or equal left it will lie in left side of pivot  else its on right
-        elif nums[left]<=target:
-            return binary_search(left,pivot,target)
+        if nums[left]<nums[right]:
+            return binary_search(left,right)
+        elif nums[left]>target:
+            return binary_search(pivot,len(nums)-1)
         else:
-            return binary_search(pivot,right,target)     
+            return binary_search(0,pivot)
+
+
+        
+            
+# class Solution(object):
+#     def search(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: int
+#         """
+#         def detect_rotation(left,right):
+
+#             if nums[left]<=nums[right]:
+#                 return left
+
+#             middle = 0
+#             while left<=right:
+
+#                 middle = (left+right)//2
+
+#                 if nums[middle]>nums[middle+1]:
+#                     break
+#                 else:
+#                     if nums[left] < nums[middle]:
+#                         left = middle+1
+#                     else:
+#                         right = middle-1
+
+#             return middle+1
+
+#         def binary_search(left,right,target):
+
+#             while left<=right:
+
+#                 middle = (left+right)//2
+
+#                 if  nums[middle]==target:
+#                     return middle
+#                 else:
+#                     if nums[middle] >= target:
+#                         right = middle-1
+#                     else:
+#                         left = middle+1
+
+#             return -1           
+
+#         left, right = 0, len(nums)-1
+#         pivot = detect_rotation(left,right)
+#         print(pivot)
+
+#         if nums[left]<=nums[right]:
+#             return binary_search(left,right,target)
+#         #### compare with target why? bcz if target is grter than or equal left it will lie in left side of pivot  else its on right
+#         elif nums[left]<=target:
+#             return binary_search(left,pivot,target)
+#         else:
+#             return binary_search(pivot,right,target)     
 
 
 
