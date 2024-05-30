f4355249ba99e64f43d5e629e2c72d3da9bd12aa
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-05-30 10:57:53 -0700

    Update Word Search.py

--- backtracking/Word Search.py
+++ backtracking/Word Search.py
@@ -1,63 +1,37 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
 
-        R, C = len(board), len(board[0])
-        start = word[0]
-        starting = []
+        m , n = len(board), len(board[0])
         visited = set()
-        i = 0
 
-        for r in range(R):
-            for c in range(C):
-                if start == board[r][c]:
-                    starting.append([r,c])
+        def backtracking(r,c,k):
 
-        ans = False
+            if k == len(word):
+                return True
+      
+            if r<0 or c<0 or c>=n or r>=m or (r,c) in visited:
+                return False
+    
+            if board[r][c]==word[k]:
 
-        def dfs(r,c,match,i):
-            
-            nonlocal ans,visited
+                visited.add((r,c))
 
-            if (r,c) in visited:
-                return
+                down = dfs(r+1,c,k+1)
+                up = dfs(r-1,c,k+1)
+                right = dfs(r,c+1,k+1)
+                left = dfs(r,c-1,k+1)
 
-            if match-1 == 0:
-                ans = True 
-                return 
+                visited.remove((r,c))
 
-            visited.add((r,c))        
+                if down or up or right or left:
+                    return True
+       
+            return False
 
-            if r+1<R and board[r+1][c] == word[i+1]:
-           
-                dfs(r+1,c,match-1,i+1)
-                
-
-            if c+1<C and board[r][c+1] == word[i+1]:
-                dfs(r,c+1,match-1,i+1)
- 
-
-            if r>=1 and board[r-1][c] == word[i+1]:
-                dfs(r-1,c,match-1,i+1) 
-
-
-            if c>=1 and board[r][c-1] == word[i+1]:
-                dfs(r,c-1,match-1,i+1)
-
-            visited.remove((r,c))
-
-        for i,j in starting:
-            dfs(i,j,len(word),0)
-            visited = set()
-            if ans == True:
-                return ans
-
-        return ans
-                    
-
-
-
-
-
-                      
-
+        for i in range(m):
+            for j in range(n):
+                if dfs(i,j,0):
+                    return True
+        
+        return False
         
