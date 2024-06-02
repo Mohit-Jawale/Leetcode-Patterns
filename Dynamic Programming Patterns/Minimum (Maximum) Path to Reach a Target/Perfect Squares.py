aa97abdd387a8f856b3494917e44c6a325d03124
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-06-02 11:50:31 -0700

    Update Perfect Squares.py

--- Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Perfect Squares.py
+++ Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/Perfect Squares.py
@@ -2,46 +2,84 @@
 class Solution:
     def numSquares(self, n: int) -> int:
 
-        perfectSquares = [i for i in range(n,0,-1) if int(sqrt(i))*int(sqrt(i))==i]
+        perfectSq = [i for i in range(1,n+1) if int(sqrt(i))*int(sqrt(i)) == int(i)]
+
         memo = {}
+        def dfs(total):
 
-        def dfs(n):
-            if n==0:
-                return 0
-            if n<0:
+            if total>n:
                 return float('inf')
-            if n in memo:
-                return memo[n]
-            minSQ = float('inf')
+            if total == n:
+                return 0
+            if total in memo:
+                return memo[total]
 
-            for i in perfectSquares:
+            minSq = float('inf')
+
+            for sq in perfectSq:
+                minSq = min(dfs(total+sq)+1,minSq)
+            
+            memo[total] = minSq
+            return minSq
+
+        return dfs(0)
+
+        dp = [float('inf')]*(n+1)
+        dp[n]=0
+        
+        for total in reversed(range(len(dp)-1)):
+    
+            for sq in perfectSq:
+                if total+sq<=n:
+                    dp[total]= min(dp[total+sq]+1,dp[total])
+                    
+        return dp[0]
+
+        
+
+# class Solution:
+#     def numSquares(self, n: int) -> int:
+
+#         perfectSquares = [i for i in range(n,0,-1) if int(sqrt(i))*int(sqrt(i))==i]
+#         memo = {}
+
+#         def dfs(n):
+#             if n==0:
+#                 return 0
+#             if n<0:
+#                 return float('inf')
+#             if n in memo:
+#                 return memo[n]
+#             minSQ = float('inf')
+
+#             for i in perfectSquares:
  
-                minSQ = min(minSQ,dfs(n-i)+1)
+#                 minSQ = min(minSQ,dfs(n-i)+1)
 
-            memo[n]= minSQ
-            return minSQ  
+#             memo[n]= minSQ
+#             return minSQ  
 
-        return dfs(n)            
+#         return dfs(n)            
 
-### 2 dp
+# ### 2 dp
 
-class Solution:
-    def numSquares(self, n: int) -> int:
+# class Solution:
+#     def numSquares(self, n: int) -> int:
 
-        perfectSquares = [i for i in range(1,n+1) if int(sqrt(i))*int(sqrt(i))==i]
+#         perfectSquares = [i for i in range(1,n+1) if int(sqrt(i))*int(sqrt(i))==i]
 
-        dp = [[0 for col in range(n+1)] for row in range(len(perfectSquares))]
+#         dp = [[0 for col in range(n+1)] for row in range(len(perfectSquares))]
 
-        for i in range(len(dp)):
-            for j in range(len(dp[0])):
+#         for i in range(len(dp)):
+#             for j in range(len(dp[0])):
 
-                if i==0 or j==0:
-                    dp[i][j]=j
-                elif j==0:
-                    continue
-                elif j>=perfectSquares[i]:
-                     dp[i][j]= min(dp[i-1][j],dp[i][j-perfectSquares[i]]+1)
-                else:
-                    dp[i][j]= dp[i-1][j]    
+#                 if i==0 or j==0:
+#                     dp[i][j]=j
+#                 elif j==0:
+#                     continue
+#                 elif j>=perfectSquares[i]:
+#                      dp[i][j]= min(dp[i-1][j],dp[i][j-perfectSquares[i]]+1)
+#                 else:
+#                     dp[i][j]= dp[i-1][j]    
 
-        return dp[-1][-1]
+#         return dp[-1][-1]
