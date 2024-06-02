3a8e11643a47fd69b8a3169ec15c4e32f8a6f36a
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-06-02 12:27:43 -0700

    Update 2 Keys Keyboard.py

--- Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/2 Keys Keyboard.py
+++ Dynamic Programming Patterns/Minimum (Maximum) Path to Reach a Target/2 Keys Keyboard.py
@@ -1,28 +1,59 @@
+
 class Solution:
     def minSteps(self, n: int) -> int:
-        
-        visited = set()
 
-        def dfs(store,display):
-      
-            if len(display)==n:
+        memo = {}
+        def dfs(i,copied):
+
+            if i==n:
                 return 0
-                
-            if len(display)>n:
-                return float('inf')   
-
-            if (store,display) in visited:
+            if i>n:
                 return float('inf')
-            else:
-                visited.add((store,display))         
+
+            if (i,copied) in memo:
+                return memo[(i,copied)]
+            
+            copyAll = float('inf')
+            if i != copied:
+                copyAll = dfs(i,i)+1
+            
+            paste = float('inf')
+            if copied>0:
+                paste = dfs(i+copied,copied)+1
+
+            minSteps= min(copyAll,paste)
+
+            memo[(i,copied)] = minSteps
+
+            return minSteps
+
+
+        return dfs(1,0)
+# class Solution:
+#     def minSteps(self, n: int) -> int:
+        
+#         visited = set()
+
+#         def dfs(store,display):
+      
+#             if len(display)==n:
+#                 return 0
+                
+#             if len(display)>n:
+#                 return float('inf')   
+
+#             if (store,display) in visited:
+#                 return float('inf')
+#             else:
+#                 visited.add((store,display))         
             
 
-            copyAll = dfs(display,display) + 1
+#             copyAll = dfs(display,display) + 1
 
-            pasteAll = dfs(store,display+store) + 1
+#             pasteAll = dfs(store,display+store) + 1
 
-            minOp = min(copyAll,pasteAll)
+#             minOp = min(copyAll,pasteAll)
 
-            return minOp
+#             return minOp
 
-        return dfs('','A')    
+#         return dfs('','A')    
