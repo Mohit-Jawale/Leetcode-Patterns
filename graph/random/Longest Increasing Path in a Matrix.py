477c766746351794efbd932a3c7c34b74dc5469d
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-06-11 15:38:24 -0700

    Update Longest Increasing Path in a Matrix.py

--- graph/random/Longest Increasing Path in a Matrix.py
+++ graph/random/Longest Increasing Path in a Matrix.py
@@ -1,3 +1,55 @@
+### topolofgical sort
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+
+        m,n = len(matrix),len(matrix[0])
+
+        moves = [(1,0),(-1,0),(0,-1),(0,1)]
+
+        adjList = defaultdict(list)
+        indegree = defaultdict(int)
+
+        for i in range(m):
+            for j in range(n):
+                
+                for r,c in moves:
+                    X,Y = i+r,j+c
+                    if X>=0 and Y>=0 and X<m and Y<n and matrix[X][Y]>matrix[i][j]:
+                        adjList[(i,j)].append((X,Y))
+                        indegree[(X,Y)]+=1
+        
+        queue = collections.deque([])
+
+        for i in range(m):
+            for j in range(n):
+                if (i,j) not in indegree:
+                    queue.append((i,j))
+
+        LIPM = 0
+
+        while queue:
+            l = len(queue)
+            for _ in range(l):
+                i,j = queue.popleft()
+                for r,c in adjList[(i,j)]:
+                    indegree[(r,c)]-=1
+                    if indegree[(r,c)]==0:
+                        queue.append((r,c))
+            
+            LIPM+=1
+
+        return LIPM
+            
+
+
+
+
+
+
+
+
+### dfs+ memo
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
 
