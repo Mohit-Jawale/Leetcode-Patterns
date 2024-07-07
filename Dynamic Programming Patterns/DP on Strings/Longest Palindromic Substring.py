9f7a7530591a800ffa41c1132583e1e50fb0110b
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-07-07 14:37:29 -0700

    Update Longest Palindromic Substring.py

--- Dynamic Programming Patterns/DP on Strings/Longest Palindromic Substring.py
+++ Dynamic Programming Patterns/DP on Strings/Longest Palindromic Substring.py
@@ -1,6 +1,45 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
+        dp = [[False for j in range(len(s))] for i in range(len(s))]
+
+        LPSub = s[0]
+
+        ###Base Case 1 with one letter
+        for i in range(len(s)):
+            dp[i][i] = True
+         
+        
+        ### Base Case 2 with two letter
+        for i in range(len(s)-1):
+            dp[i][i+1] = (s[i]==s[i+1])
+            LPSub = s[i:i+2] if dp[i][i+1] else LPSub
+        
+        for length in range(3,len(s)+1):
+            i = 0
+            for j in range(length-1,len(s)):
+
+                dp[i][j] = dp[i+1][j-1] and s[i]==s[j]
+
+                LPSub = s[i:j+1] if dp[i][j] else LPSub
+                
+                i+=1
+
+        return LPSub
+
+
+
+
+
+
+        
+
+
+        
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
         def checkPalindrome(left,right):
 
             while left>=0 and right<len(s) and s[left]==s[right]:
