c3151b5bc38383e60142ba5e7325e5779dadc676
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-07-07 14:08:17 -0700

    Update Palindromic Substrings.py

--- Dynamic Programming Patterns/DP on Strings/Palindromic Substrings.py
+++ Dynamic Programming Patterns/DP on Strings/Palindromic Substrings.py
@@ -1,3 +1,36 @@
+### here the third for loop is going zigzag 
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        
+
+        count = 0
+
+        dp = [ [ False for j in range(len(s))] for i in range(len(s)) ]
+
+        ### Base case with length 1
+
+        for i in range(len(s)):
+            dp[i][i] = True
+            count+=1
+        
+        ### Base case with length 2
+
+        for i in range(len(s)-1):
+            dp[i][i+1] = (s[i]==s[i+1])
+            count+= dp[i][i+1]
+        
+        ### now we find out remaining length string 3,4 ....
+        for length in range(3,len(s)+1):
+            i = 0
+            for j in range(length-1,len(s)):
+                dp[i][j] = dp[i+1][j-1] and (s[i] == s[j])
+                count+=dp[i][j]
+                i+=1
+
+        return count
+
+
+
 ### This question has trick this can be done in dp way but this way is easier and intutive to understand
 class Solution:
     def countSubstrings(self, s: str) -> int:
