3c07cccb568d2aa34024aa37eb8c0b37d43b5b05
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-07-10 18:07:30 -0700

    Update Longest Palindromic Subsequence.py

--- Dynamic Programming Patterns/DP on Strings/Longest Palindromic Subsequence.py
+++ Dynamic Programming Patterns/DP on Strings/Longest Palindromic Subsequence.py
@@ -1,31 +1,46 @@
 ### DF +memo
 ### the LPS is basically reverse of the str and LCS on the both string
 ### the longest palindromic subsequence of string is reverse of its own and LCS b/w them.--this is trick
-class Solution:
-    def longestPalindromeSubseq(self, s: str) -> int:
-
         memo = {}
-        revstr = s[::-1]
 
         def dfs(i,j):
 
+            if i==j:
+                return 1
+            if i>j:
+                return 0
             if (i,j) in memo:
                 return memo[(i,j)]
-
-            if i>=len(s) or j>=len(s):
-                return 0
-
-            LPS = 0
-
-            if s[i]==revstr[j]:
-                LPS = dfs(i+1,j+1)+1
-
+            
+            lps = 0
+            
+            if s[i]==s[j]:
+                lps = max(dfs(i+1,j-1)+2,lps)
             else:
-                LPS = max(dfs(i+1,j),dfs(i,j+1))
+                lps = max(dfs(i,j-1),dfs(i+1,j))
+            
+            memo[(i,j)] = lps
+            return lps
+        
+        return dfs(0,len(s)-1)
 
-            memo[(i,j)] = LPS
-            return LPS
 
+### 2D - dp
 
-        return dfs(0,0) 
+        dp = [[0 for _ in range(len(s)+1)] for _ in range(len(s)+1)]
+        
+        for i in range(len(s)):
+            dp[i][i]= 1
+
+        for i in reversed(range(len(s))):
+            for j in range(i+1, len(s)):
+         
+                if s[i]==s[j]:
+                    dp[i][j] = max(dp[i+1][j-1]+2,dp[i][j])
+                else:
+                    dp[i][j]= max(dp[i+1][j],dp[i][j-1])
+        
+
+        return dp[0][len(s)-1]
+
         
