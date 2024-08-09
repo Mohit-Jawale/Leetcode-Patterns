5c9fc0d45c6504760040bdef2c3f2e651a489665
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-08-09 13:41:40 -0700

    Create Number of Distinct Islands.py

---
+++ Amazon/Number of Distinct Islands.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+
+        unique_island = set()
+
+        m,n = len(grid),len(grid[0])
+
+
+        def dfs(r,c,direction):
+
+            nonlocal path_signature
+            
+            if r<0 or c<0 or r>=m or c>=n:
+                return ""
+            
+            if grid[r][c]==1 and grid[r][c]!='#':
+                grid[r][c] = '#'
+                path_signature.append(direction)
+                dfs(r+1,c,'D')
+                dfs(r-1,c,'U')
+                dfs(r,c+1,'R')
+                dfs(r,c-1,'L')
+                path_signature.append('*')
+
+        
+        for i in range(m):
+            for j in range(n):
+
+                if grid[i][j]==1:
+                    path_signature = []
+                    dfs(i,j,'*')
+
+                    unique_island.add(tuple(path_signature))
+        print(unique_island)
+        return len(unique_island)
