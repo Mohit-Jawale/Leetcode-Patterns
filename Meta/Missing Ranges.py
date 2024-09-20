f80a03cf1754e90c84a0ed2a8c99a6a388306983
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-09-20 11:17:08 -0700

    Create Missing Ranges.py

---
+++ Meta/Missing Ranges.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+
+        ans = []
+
+
+        for num in nums:
+
+            if lower<num:
+                ans.append([lower,num-1])
+            
+            lower = num+1
+        
+        if lower<=upper:
+            ans.append([lower,upper])
+        
+        return ans
+
+
+
+        
+        
