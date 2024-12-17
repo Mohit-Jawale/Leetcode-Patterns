838d52e47bb3e0f98226b70fa0bbeff03900b16b
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2024-12-17 14:24:55 -0800

    Update Climbing Stairs.py

--- Dynamic Programming Patterns/Distinct Ways/Climbing Stairs.py
+++ Dynamic Programming Patterns/Distinct Ways/Climbing Stairs.py
@@ -1,4 +1,38 @@
 ### DFS + memo
+        memo = {}
+
+        def dfs(currStep):
+
+            if currStep==n:
+                return 1
+            if currStep>n:
+                return 0
+            if currStep in memo:
+                return memo[currStep]
+            
+            total = dfs(currStep+1) + dfs(currStep+2)
+            memo[currStep] = total
+
+            return total
+        
+
+        return dfs(0)
+
+
+## 1 DP
+        dp = [0]*(n+2)
+        dp[n]=1
+
+        for i in reversed(range(n)):
+
+            dp[i]=dp[i+1]+dp[i+2]
+        
+        return dp[0]
+
+        
+
+
+### DFS + memo
 class Solution:
     def climbStairs(self, n: int) -> int:
         
