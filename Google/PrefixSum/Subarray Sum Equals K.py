f3ff1500a866b9718a1bab91e68eda4377c28923
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2025-05-19 10:46:55 -0700

    Create Subarray Sum Equals K.py

---
+++ Google/PrefixSum/Subarray Sum Equals K.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+
+        lookup = collections.defaultdict(int)
+
+        lookup[0] = 1 ###3 this is base case [1,1,1,1,1,1] k=3 so here at i=2 we will have 1 arr with sum 0 previously
+        prefixSum = 0
+        res = 0
+
+        for i in nums:
+            prefixSum+=i
+            curr_sum = prefixSum - k
+            if curr_sum in lookup:
+                res+=lookup[curr_sum]
+            lookup[prefixSum]+=1
+        
+        return res
