b980410a379551faf911283a7615ce786e86e5ca
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2025-11-19 07:56:16 -0800

    Create Network Delay Time.py

---
+++ bloomberg/Network Delay Time.py
@@ -0,0 +1,32 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+
+        adj_list = defaultdict(list)
+
+        for s,d,w in times:
+            adj_list[s].append((d,w))
+
+        minHeap = [(0,k)]
+        visited = set()
+        t = 0
+
+        while minHeap:
+
+            w1,node = heapq.heappop(minHeap)
+            
+            if node in visited:## this is imp
+                continue
+
+            visited.add(node)
+            t = max(w1,t)
+            for neighbour in adj_list[node]:
+                if neighbour[0] not in visited:
+                    heapq.heappush(minHeap,(w1+neighbour[1],neighbour[0]))
+
+        return t if len(visited)==n else -1           
+
+
+
+
+
+        
