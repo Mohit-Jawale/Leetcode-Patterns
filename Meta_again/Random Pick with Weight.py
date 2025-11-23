4f9b51b25f45e4f74f5685bce7485d53956a2505
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2025-11-23 12:05:24 -0800

    Create Random Pick with Weight.py

---
+++ Meta_again/Random Pick with Weight.py
@@ -0,0 +1,38 @@
+import random
+class Solution:
+
+    def __init__(self, w: List[int]):
+
+        self.total_weight = 0
+
+        self.prefix_sum = []
+
+        for weight in w:
+            self.total_weight+=weight
+            self.prefix_sum.append(self.total_weight)
+
+        
+    def pickIndex(self) -> int:
+
+        target = random.randint(1,self.total_weight)
+
+        left,right = 0,len(self.prefix_sum)
+
+        while left<right:
+
+            mid = left+(right-left)//2
+
+            if target>self.prefix_sum[mid]:
+                left = mid+1
+            else:
+                right = mid
+                
+        return left
+
+
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
